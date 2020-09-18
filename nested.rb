9b594b00a8795009eedbc53259ff04682bf02147
Carl J <carlj1881@yahoo.com>
2020-09-18 19:44:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {:yukihiro_matsumoto => {:known_for => "Ruby",  :languages => ["LISP", "C"]}
+programmer_hash[:yukihiro_matsumoto] = {:yukihiro_matsumoto => {:known_for => "Ruby",  :languages => ["LISP", "C"]}}
 end
-end
+
 
 
 def changing_alan
