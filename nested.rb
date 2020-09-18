3a86bb5d1e4aa0d387f2eb5cb6a10e005e5f21f7
Carl J <carlj1881@yahoo.com>
2020-09-18 19:06:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,10 +69,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -86,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+      programmer_hash
 end
 
 def changing_alan
@@ -107,8 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for].delete(:languages)
-     programmer_hash[:alan_kay][:known_for][:alans_new_info][:alans_new_info]
+     
 end
 
 def adding_to_dennis
