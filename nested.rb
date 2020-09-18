1a4f77ef4bc34a2945c6a7db8ae379529d3a8132
Carl J <carlj1881@yahoo.com>
2020-09-18 20:25:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,8 @@ def changing_alan
      #programmer_hash.delete(:known_for)
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash
+     
 end
 
 def adding_to_dennis
